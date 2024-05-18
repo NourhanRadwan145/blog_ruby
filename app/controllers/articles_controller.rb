4074f767494880f7a1e29570ce33d0d1bf82d212
NourhanRadwan145 <nourshaheen170@gmail.com>
2024-05-18 23:18:13 +0300

    refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "nour", password: "nour", except: [:index, :show]
   def index
     @articles = Article.all
   end
@@ -12,7 +13,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
@@ -43,7 +44,8 @@ class ArticlesController < ApplicationController
   end
 
   private
-  def article_params
-    params.require(:article).permit(:title, :body)
-  end
+    def article_params
+      params.require(:article).permit(:title, :body, :status)
+    end
+
 end
